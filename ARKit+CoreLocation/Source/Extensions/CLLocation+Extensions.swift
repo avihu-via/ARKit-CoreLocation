1246ffac18ea8205cab3eeae1f06412c8b828dee
Avihu Turzion <avihu.turzion@gmail.com>
2018-03-05 22:00:50 +0200

    Removed synesthizeable constructor

--- ARKit+CoreLocation/Source/Extensions/CLLocation+Extensions.swift
+++ ARKit+CoreLocation/Source/Extensions/CLLocation+Extensions.swift
@@ -14,12 +14,6 @@ public struct LocationTranslation {
     public var latitudeTranslation: Double
     public var longitudeTranslation: Double
     public var altitudeTranslation: Double
-    
-    public init(latitudeTranslation: Double, longitudeTranslation: Double, altitudeTranslation: Double) {
-        self.latitudeTranslation = latitudeTranslation
-        self.longitudeTranslation = longitudeTranslation
-        self.altitudeTranslation = altitudeTranslation
-    }
 }
 
 public extension CLLocation {
