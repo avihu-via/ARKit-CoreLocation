e8f49b26a91bee0008b6c9d62c19e82c86c90068
Avihu Turzion <avihu.turzion@gmail.com>
2018-03-05 21:54:05 +0200

    Added unit-tests target

---
+++ ARKit+CoreLocationTests/ARKitCoreLocationTests.swift
@@ -0,0 +1,12 @@
+//
+//  ARKit_CoreLocationTests.swift
+//  ARKit+CoreLocationTests
+//
+//  Created by Avihu Turzion on 05/03/2018.
+//  Copyright © 2018 Project Dent. All rights reserved.
+//
+
+import XCTest
+
+class ARKitCoreLocationTests: XCTestCase {
+}
